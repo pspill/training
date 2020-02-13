eba3f28832abfc1a27780727e03421c7166fc529
Peter Spillman <peter.spillman@disney.com>
2020-02-13 10:10:06 -0500

    transforms

--- ottergram/scripts/main.js
+++ ottergram/scripts/main.js
@@ -2,7 +2,11 @@
 
 const DETAIL_IMAGE_SELECTOR = '[data-image-role="target"]';
 const DETAIL_TITLE_SELECTOR = '[data-image-role="title"]';
+const DETAIL_FRAME_SELECTOR = '[data-image-role="frame"]';
 const THUMBNAIL_LINK_SELECTOR = '[data-image-role="trigger"]';
+const HIDDEN_DETAILS_CLASS = 'hidden-detail';
+const TINY_EFFECT_CLASS = 'is-tiny';
+const ESC_KEY = 27;
 
 function setDetails(imageUrl, titleText) {
     let detailImage = document.querySelector(DETAIL_IMAGE_SELECTOR);
@@ -27,6 +31,7 @@ function addThumbClickedHanlder(thumbnail) {
     thumbnail.addEventListener('click', function(event) {
         event.preventDefault();
         setDetailsFromThumb(thumbnail);
+        showDetails();
     });
 }
 
@@ -35,9 +40,31 @@ function getThumbnailsArray() {
     return Array.from(thumbnails);
 }
 
+function addKeyPressHandler() {
+    document.body.addEventListener('keyup', function(event) {
+        event.preventDefault();
+        console.log(event.keyCode);
+        if (event.keyCode === ESC_KEY) {
+            hideDetails();
+        }
+    });
+}
+
 function initializeEvents() {
     let thumbnails = getThumbnailsArray();
     thumbnails.forEach(addThumbClickedHanlder);
+    addKeyPressHandler();
+}
+
+function hideDetails() {
+    document.body.classList.add(HIDDEN_DETAILS_CLASS);
+}
+
+function showDetails() {
+    var frame = document.body.querySelector(DETAIL_FRAME_SELECTOR);
+    document.body.classList.remove(HIDDEN_DETAILS_CLASS);
+    frame.classList.add(TINY_EFFECT_CLASS);
+    setTimeout(function(){frame.classList.remove(TINY_EFFECT_CLASS)}, 0);
 }
 
 initializeEvents();
